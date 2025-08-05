c3ddcbd51292976419e98046f026e8e2e0251105
WSLinme <brokenmopflag@gmail.com>
2025-08-05 21:18:01 +0100

    Added dynamic movie display from function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,25 @@ import MovieCard from "../components/MovieCard"
 
 
 function Home() {
+    
+    const HandleSearch = () => {
+        alert()
+    }
+
     const movies = [
         {id: 1, title: "John Wick", release_date: "2024"},
         {id: 2, title: "Hello Kitty", release_date: "2025"},
-        {id: 3, title: "Joshua the Giant", release_date: "2026"}
+        {id: 3, title: "Joshua the Giant", release_date: "2026"},
+        {id: 4, title: "Joshua the Giant", release_date: "2027"}
     ]
 
 
     return (
-        <div>
-            <div>
-                <MovieCard movie={{title: "Tim's Films", release_date: "2024"}} />
-                <MovieCard movie={{title: "Hello", release_date: "2025"}} />
+        <div className="home">
+            <div className="movie-grid">
+                {movies.map(movie => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))} 
             </div>
         </div>
     )
